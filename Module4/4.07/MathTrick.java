27bb648ee9440f40223eb02557b0cb227cba2549
PorkSchool <114511318+PorkSchool@users.noreply.github.com>
2023-10-31 12:43:52 +0000

    Started 4.07

---
+++ Module4/4.07/MathTrick.java
@@ -0,0 +1,31 @@
+class MathTrick{
+
+
+
+    //a. Generate a random 3-digit number so that the first and third digits differ by more than one.
+    //b. Now reverse the digits to form a second number.
+    //c. Subtract the smaller number from the larger one.
+    //d. Now reverse the digits in the answer you got in step c and add it to that number.
+    //e. Multiply by one million.
+    //f. Subtract 733,361,573.
+        //Hint: since replaceLtr is expecting a String, you should use String.valueOf(number) to create a String variable from the integer
+    //variable before step g.
+    //g. Then, replace each of the digits in your answer, with the letter it corresponds to using the following table:
+    // 0 --> Y
+    // 1 --> M
+    // 2 --> P
+    // 3 --> L
+    // 4 --> R
+    // 5 --> O
+    // 6 --> F
+    // 10/31/23, 8:34 AM AP Computer Science A
+    // https://learn.flvs.net/educatorng/student/mjoslin6/5178/frame-toolbar?frame_url=https:%2F%2Flearn.flvs.net%2Fwebdav%2Feducator_apcomputersciencea_v20%2Findex.htm 2/2
+    // 7 --> A
+    // 8 --> I
+    // 9 --> B
+    //h. Now reverse the letters in the string to read your message backward.
+
+    public static void main(String[] args){
+
+    }
+}
