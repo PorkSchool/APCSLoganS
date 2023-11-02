3ca40cfaf8b1cc731d1f53696c9359e133dc617e
PorkSchool <114511318+PorkSchool@users.noreply.github.com>
2023-11-02 14:24:21 +0000

    AAAAAAAAAAAAAA

--- Module4/4.06/SecretPasscode.java
+++ Module4/4.06/SecretPasscode.java
@@ -6,33 +6,22 @@
 * This program will create a secret passcode and output it to a text file
 */
 import java.io.IOException;
+
 import java.io.PrintWriter;
 import java.io.File;
 import java.util.Scanner;
 
 public class SecretPasscode {
-   public static String MakePassword(int len, String password){
-      String pas = "";
-
-      for (int i = 0; i < len; i++) {
-
-         //Numbers  48-57
-         //lowercase 97-122
-         //upper case  65-90
-         int ranNumber = (int) (Math.random() * (126 - 33 + 1) + 33);
-         if(!(ranNumber>=48 && ranNumber<=57) || !(ranNumber>=97 && ranNumber<=122) || !(ranNumber>=65 && ranNumber<=90)){
-            ranNumber = (int)(Math.random() * (126 - 33 + 1) + 33);
-         }
-         char ranChar = (char) (ranNumber);        
-         pas += ranChar;
-      }
-      System.out.println("Your Password has been created and written to a text file");
-      return pas;
-   }
-
+   
    public static void main(String[] args) throws IOException {
       String password = "";
       File theMainFile = new File("PasswordOutput.txt");
+
+      //error correction
+      if (!theMainFile.exists()) {
+         System.out.println("MAIN FILE DOES NOT EXIST");
+         theMainFile.createNewFile();
+      }
 
       Scanner readFile = new Scanner(theMainFile);
 
@@ -64,8 +53,7 @@ public class SecretPasscode {
       choice = in.next();
       choice = choice.substring(0, 1);
       while(choice.equalsIgnoreCase("Y")){
-
-
+         
          System.out.println("Please Provide a length for your password (Minimum 6): ");
          pwLength = Integer.parseInt(in.next());
          while (pwLength < 6) {
@@ -82,15 +70,37 @@ public class SecretPasscode {
          choice = choice.substring(0, 1);
       }
    
-      outFile.close();
    
       System.out.println("Here is a list of your passwords:");
+      
+
       int i = 0;
       while(readFile.hasNext()){
          i++;
          System.out.println(i+": " +readFile.next());
       }
+
       readFile.close();
+      outFile.close();
       in.close();
+   }
+
+   public static String MakePassword(int len, String password){
+      String pas = "";
+
+      for (int i = 0; i < len; i++) {
+
+         //Numbers  48-57
+         //lowercase 97-122
+         //upper case  65-90
+         int ranNumber = (int) (Math.random() * (126 - 33 + 1) + 33);
+         if(!(ranNumber>=48 && ranNumber<=57) || !(ranNumber>=97 && ranNumber<=122) || !(ranNumber>=65 && ranNumber<=90)){
+            ranNumber = (int)(Math.random() * (126 - 33 + 1) + 33);
+         }
+         char ranChar = (char) (ranNumber);        
+         pas += ranChar;
+      }
+      System.out.println("Your Password has been created and written to a text file");
+      return pas;
    }
 }
