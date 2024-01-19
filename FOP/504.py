7d8bcec89ef5838137e7a1d73937a4fbc0f01e24
PorkSchool <114511318+PorkSchool@users.noreply.github.com>
2024-01-19 18:19:40 +0000

    magpie

--- FOP/504.py
+++ FOP/504.py
@@ -1,3 +1,6 @@
+#Logan Santamarina 1/18/23 Superhero creotr and outputter
+
+
 # Superhero class represents the facts related to a superhero.
 class Superhero:
     def __init__(self, _name = "", _strengthPts = 0, _speed =0, _accuracy=0):
@@ -8,7 +11,7 @@ class Superhero:
         self.strengthPts = _strengthPts
 
         self.speed = _speed
-        seld.accuracy = _accuracy
+        self.accuracy = _accuracy
 
     def addStrengthPts(self, points):
         # Adds points to the superhero's strength.
@@ -27,3 +30,7 @@ def main():
     speedyBoy.addAccuracy(50)
     speedyBoy.slowDown(20)
 
+    paragraph = f"{speedyBoy.name} is an incredible superhero known for his extraordinary abilities. Possessing the powers of accuracy {speedyBoy.accuracy}, strength {speedyBoy.strengthPts}, and speed {speedyBoy.speed}, he is a force to be reckoned with. Whether it's lifting heavy objects with unmatched strength {speedyBoy.strengthPts}, zooming through the city with incredible speed {speedyBoy.speed}, or hitting targets with pinpoint accuracy {speedyBoy.accuracy}, SpeedyBoy is always ready to save the day. His unique combination of strength, speed, and accuracy makes him a true guardian of justice and a symbol of heroism in the city."
+
+    print(paragraph)
+main()
