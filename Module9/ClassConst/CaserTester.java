af24f2432aeeb8333db6064ba17fdd93573f2b86
PorkSchool <114511318+PorkSchool@users.noreply.github.com>
2024-03-06 18:10:58 +0000

    started 904

--- Module9/ClassConst/CaserTester.java
+++ Module9/ClassConst/CaserTester.java
@@ -1,4 +1,11 @@
 
 public class CaserTester {
-    
+
+    public int shift;
+
+    public static void main(String[] args) {
+        Encryption enc = new Encryption("I like food");
+        System.out.println(enc.ALPHABET);
+        enc.EncryptAlphabet(3);
+    }
 }
--- Module9/ClassConst/Encryption.java
+++ Module9/ClassConst/Encryption.java
@@ -1,4 +1,29 @@
-
 public class Encryption {
-    
+    private String message;
+    public final String ALPHABET = "abcdefghijklmnopqrstuvwxyz";
+
+    public Encryption(String m) {
+        message = m;
+    }
+
+    public void EncryptAlphabet(int shift){
+        StringBuilder cipherBet = new StringBuilder();
+
+        for(int i =0; i <ALPHABET.length();i++){
+            char letter = ALPHABET.charAt(i);
+            int shiftIndex = (i+shift) % ALPHABET.length();
+            char shiftedChar = ALPHABET.charAt(shiftIndex);
+
+            cipherBet.append(shiftedChar);
+        }
+        System.out.println(cipherBet);
+    }
+    public void EncryptMessage(int shift) {
+        char[] cMes = message.toCharArray();
+        System.out.println(message);
+        for (char c : cMes) {
+            
+        }
+        System.out.println(message);
+    }
 }
